3c9ed43926a43d0c6ea3160bab851ef117d46165
tjf801 <fuscofamily0@gmail.com>
2023-02-28 13:43:12 -0500

    rework vision subsystem

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -5,6 +5,7 @@
 package frc.robot.subsystems;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.photonvision.PhotonCamera;
 import org.photonvision.targeting.PhotonPipelineResult;
@@ -14,45 +15,61 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Vision extends SubsystemBase {
-    /** Creates a new Vision. */
-    public PhotonCamera camera = new PhotonCamera(Constants.USB_CAMERA_NAME);
-    boolean hasTarget;    //True means target detected; false means no target detected
-    PhotonPipelineResult result;    //sores all collected Photonvision data
+    private final PhotonCamera camera = new PhotonCamera(Constants.USB_CAMERA_NAME);
     
-    public Vision() {
-        
-    }
+    private boolean hasTarget;                // whether or not the camera has a target
+    private PhotonPipelineResult lastResult;  // stores all collected data from the camera
+    
+    /** Creates a new Vision Subsystem. */
+    public Vision() {}
     
     @Override
     public void periodic() {
-        // This method will be called once per scheduler run
         PhotonPipelineResult result = camera.getLatestResult();
         hasTarget = result.hasTargets();
-        if (hasTarget)    {
-            this.result = result;
-        }
+        // only set the lastResult variable if we actually found something
+        if (hasTarget) this.lastResult = result;
     }
     
-    public PhotonTrackedTarget getTargetWithID(int id) {    //Returns the April tage ID (if it exists)
-        List<PhotonTrackedTarget> targets = result.getTargets();
-        for (PhotonTrackedTarget i : targets)    {
-            if (i.getFiducialId() == id)    {
-                return i;
+    /**
+     * Returns the target with the given ID, if it exists.
+     * <p>
+     * NOTE: this function returns a {@code java.util.Optional<PhotonTrackedTarget>}, which
+     *       is a special type of object that can either contain a value or be empty.
+     *       As such, you have to use the {@code isPresent()} and {@code get()} methods 
+     *       to access the actual value. 
+     *       This is to prevent any accidental null pointer exceptions.
+     * 
+     * @param id The ID of the target to find.
+     * @return The target with the given April tage ID, if it exists.
+     */
+    public Optional<PhotonTrackedTarget> getTargetWithID(int id) {
+        List<PhotonTrackedTarget> targets = lastResult.getTargets();
+        
+        for (PhotonTrackedTarget target : targets)    {
+            if (target.getFiducialId() == id)    {
+                return Optional.of(target);
             }
         }
-        return null;    //No target found
+        
+        //No target found
+        return Optional.empty(); 
     }
     
-    public PhotonTrackedTarget getBestTarget() {
-        if    (hasTarget) {
-            return result.getBestTarget();
+    /**
+     * Returns the best target, if one exists.
+     * 
+     * @return The best target, as a {@code java.util.Optional<PhotonTrackedTarget>}.
+     */
+    public Optional<PhotonTrackedTarget> getBestTarget() {
+        if (hasTarget) {
+            return Optional.ofNullable(lastResult.getBestTarget());
         }
-        else {
-            return null;
-        }
+        
+        return Optional.empty();
     }
     
-    public boolean getHasTarget() {
+    public boolean hasTarget() {
         return hasTarget;
     }
 }
